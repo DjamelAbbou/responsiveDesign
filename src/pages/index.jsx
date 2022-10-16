fdeca4c8348d68aed0a4a268bba49b375873abd9
djamelabbou <djamelabbou@outlook.com>
2022-10-16 09:24:19 +0100

    upgraded the template

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,20 @@
 import Container from "@/ui/container";
+import { useState } from "react";
+
 export default function HomePage() {
   return (
     <Container className="mt-16 text-white text-2xl">Hello world</Container>
   );
+}
+
+export function withValidations(Component) {
+  return function wrappedComponent({}) {
+    const [errors, setErrors] = useState([]);
+    return (
+      <>
+        <Component />
+        {errors.length > 0 && <div>{errors.join(", ")}</div>}
+      </>
+    );
+  };
 }
