ed0eacf9651d8ef77ca120b76bc15c7dcc0af20c
djamelabbou <djamelabbou@outlook.com>
2022-10-18 21:33:02 +0100

    added buttons variants, plus used INHERITANCE in FP

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,8 @@
 import Container from "@/ui/container";
 
 export default function HomePage() {
-
   return (
-    <Container className="mt-16 text-white text-2xl">
-	Hello world
-   </Container>
+    <Container className="mt-16 text-white text-2xl">Hello world</Container>
   );
 }
 
