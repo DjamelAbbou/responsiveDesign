16000ab08fc1d26c374b39e6867b90b74379de17
djamelabbou <djamelabbou@outlook.com>
2022-11-18 13:47:20 +0100

    removed ssr to avoid bugs and dbPromise import

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,23 @@
 import Container from "@/ui/container";
-import dbPromise, { jsonify } from "@/modules/db";
+// import dbPromise, { jsonify } from "@/modules/db";
 
 export default function HomePage({ users }) {
   return <Container className="mt-16 text-white text-2xl"></Container>;
 }
 
-export async function getServerSideProps({ ctx }) {
-  const users = await (await dbPromise)
-    .db()
-    .collection("users")
-    .find({})
-    .toArray();
+// export async function getServerSideProps({ ctx }) {
+//   const users = await (await dbPromise)
+//     .db()
+//     .collection("users")
+//     .find({})
+//     .toArray();
 
-  return {
-    props: {
-      users: jsonify(users),
-    },
-  };
-}
+//   return {
+//     props: {
+//       users: jsonify(users),
+//     },
+//   };
+// }
 // export function withValidations(Component) {
 //   return function wrappedComponent({}) {
 //     const [errors, setErrors] = useState([]);
