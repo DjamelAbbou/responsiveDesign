216ebd72ce969142aa92132ae553fdaa8c5f3053
djamelabbou <djamelabbou@outlook.com>
2022-12-22 20:34:02 +0100

    changed a bit of this a bit of that

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,5 @@
 import Container from "@/ui/container";
-// import dbPromise, { jsonify } from "@/modules/db";
 
-export default function HomePage({ users }) {
-  return <Container className="mt-16 text-white text-2xl"></Container>;
+export default function HomePage() {
+  return <Container className=""></Container>;
 }
-
-// export async function getServerSideProps({ ctx }) {
-//   const users = await (await dbPromise)
-//     .db()
-//     .collection("users")
-//     .find({})
-//     .toArray();
-
-//   return {
-//     props: {
-//       users: jsonify(users),
-//     },
-//   };
-// }
-// export function withValidations(Component) {
-//   return function wrappedComponent({}) {
-//     const [errors, setErrors] = useState([]);
-//     return (
-//       <>
-//         <Component />
-//         {errors.length > 0 && <div>{errors.join(", ")}</div>}
-//       </>
-//     );
-//   };
-// }
