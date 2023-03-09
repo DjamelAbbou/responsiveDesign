9d48ad837f4bddf1c276b1fd3852064e6553b7fd
djamelabbou <djamelabbou@outlook.com>
2023-03-09 14:52:18 +0100

    please helpppp

---
+++ src/pages/challenge/3.jsx
@@ -0,0 +1,19 @@
+export default function index() {
+  return (
+    <div className="bg-[#23424A] p-8 text-white md:p-16 ">
+      <div className="md:max-w-[50%]">
+        <h1 className="mb-5 text-2xl font-bold md:text-[48px] md:leading-[1.2]">
+          Responsive layouts don't have to be a struggle
+        </h1>
+        <p className="text-sm md:text-[21px] md:leading-[1.2]">
+          this is some temporary text inorder to make things look nice and
+          pretty and simple make it design perfect pixel ok so dont mind if
+          things
+        </p>
+        <button className="mt-10 rounded-3xl bg-[#38CFD9] px-8 py-2 text-sm font-medium text-black md:text-[21px]">
+          I WANT TO LEARN
+        </button>
+      </div>
+    </div>
+  );
+}
